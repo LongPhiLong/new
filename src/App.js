c74b812ccb605d51272ed5ed980902e2a1cef1ef
LongPhiLong <gangsteridol977@gmail.com>
2022-03-21 21:37:18 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ANh thanh xoa het o dia roi</h1>
+    </div>
+  );
+}
+
+export default App;
